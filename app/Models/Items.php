6163481d017109bfc8e0e7d8837bba4e4978234b
Yamil <yarrib76@gmail.com>
2016-10-19 18:42:42 -0300

    Cambios

--- app/Models/Items.php
+++ app/Models/Items.php
@@ -22,6 +22,6 @@ class Items extends Model
         return DB::select('SELECT fecha, item_id as id, items.nombre, sum(importe) as Total FROM gastosadmin.registro_gastos inner join gastosadmin.items
         on item_id = items.id where fecha >= "'. $anio . '/' . $mes . '/01'.'" and
         fecha <= "'. $anio . '/' . $mes . '/31'.'"
-        group by items.nombre ORDER by id ASC');
+        group by items.nombre ORDER by Total desc');
     }
 }
