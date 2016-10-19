f33ef0d711799be989c73912c93b12bb9261819f
Yamil <yarrib76@gmail.com>
2016-10-19 12:31:09 -0300

    cambios

--- app/Http/Controllers/Api/ListaItemsController.php
+++ app/Http/Controllers/Api/ListaItemsController.php
@@ -6,6 +6,7 @@ use GastosAdmin\Models\Items;
 use GastosAdmin\Http\Requests;
 use GastosAdmin\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Response;
 
 class ListaItemsController extends Controller
@@ -14,6 +15,12 @@ class ListaItemsController extends Controller
       //  $listaItems = Items::all();
       //  $listaItems = $this->totalesPorItems($listaItems);
         $listaItemsTotal = new Items();
+        $anio = Input::get('anio');
+        $mes = Input::get('mes');
+        $call = Input::get('call');
+        if ($call == "full"){
+            return Response::json($listaItemsTotal->itemsTotalAnioMes($anio,$mes));
+        } 
         return Response::json($listaItemsTotal->itemsTotal());
     }
 
--- app/Models/Items.php
+++ app/Models/Items.php
@@ -15,7 +15,13 @@ class Items extends Model
     }
 
     public function itemsTotal(){
-        return DB::select('SELECT item_id as id, items.nombre, sum(importe) as Total FROM gastosadmin.registro_gastos inner join gastosadmin.items
+        return DB::select('SELECT fecha, item_id as id, items.nombre, sum(importe) as Total FROM gastosadmin.registro_gastos inner join gastosadmin.items
         on item_id = items.id group by items.nombre ORDER by id ASC');
+    }
+    public function itemsTotalAnioMes($anio,$mes){
+        return DB::select('SELECT fecha, item_id as id, items.nombre, sum(importe) as Total FROM gastosadmin.registro_gastos inner join gastosadmin.items
+        on item_id = items.id where fecha >= "'. $anio . '/' . $mes . '/01'.'" and
+        fecha <= "'. $anio . '/' . $mes . '/31'.'"
+        group by items.nombre ORDER by id ASC');
     }
 }
